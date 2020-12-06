6b8ab3fceb1600b66bdf24785dd4a647bf55d7f2
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2020-12-06 12:51:02 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+var player1name = localStorage.getItem("player1name");
+var player2name = localStorage.getItem("player2name");
+var player1score = 0;
+var player2score = 0;
+document.getElementById("player1_name").innerHTML = player1name + " : ";
+document.getElementById("player2_name").innerHTML = player2name + " : ";
+document.getElementById("player1_score").innerHTML = player1score;
+document.getElementById("player2_score").innerHTML = player2score;
+document.getElementById("player_question").innerHTML = "Question Turn: " + player1name;
+document.getElementById("player_answer").innerHTML = "Answer Turn: " + player2name;
